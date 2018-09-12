0c549b64623a202fd08b0cff4fafc1aa154fdc24
Erin M Masterson <erin.margot.masterson@gmail.com>
2018-09-11 23:00:12 -0400

    changed start script in package.json

--- expressApp.js
+++ expressApp.js
@@ -6,6 +6,8 @@ const app = express();
 
 const port = process.env.PORT || 80;
 
+app.use(express.static(__dirname + "/src/app/"));
+
 app.listen(port, function(req, res){
     res.sendFile("./src/app/app.component.html");
 });
